63e463b8f7ef80887695b01a4b5ff2f671dbb67d
Semantic <sandip.jadhav36@gmail.com>
2018-03-29 17:01:52 +0530

    initial commit

---
+++ Custom Demo/Extensions/CustomPointAnnotation.swift
@@ -0,0 +1,12 @@
+//
+//  CustomPointAnnotation.swift
+//  Custom Demo
+//
+//  Created by Semantic on 29/03/18.
+//  Copyright © 2018 Sandip Jadhav. All rights reserved.
+//
+import UIKit
+import MapKit
+class CustomPointAnnotation: MKPointAnnotation {
+    var imageName: UIImage!
+}
